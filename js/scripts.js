d43b3a25127b57fdc8fb2b31ad4499a11a6054fa
drevitsky <4227723@gmail.com>
2019-06-11 11:02:06 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+window.onload = function () {
+  
+  const a = document.querySelectorAll('.item-img');
+  const itemContent = document.querySelectorAll('.item-content');
+
+  a.forEach(function(item,i,a){
+    let index = i;
+    item.addEventListener("mouseover", function(e) {
+      // console.log(item);
+      if (item.classList.contains('mouseout')){
+          item.classList.remove('mouseout');
+      }
+      item.classList.add('mouseover');
+    });
+
+    item.addEventListener("mouseout", function(e) {
+
+      item.classList.add('mouseout');
+      item.childNodes[3].addEventListener("transitionend", function(e) {
+      
+        if (item.classList.contains('mouseout')&&item.classList.contains('mouseover')){
+          item.classList.remove('mouseover');
+        }
+      
+      })  
+     
+    });
+
+    item.addEventListener("click", function(e) {
+      // const target = e.target;
+      //    console.log(target.classList);
+
+         // console.log(item.parentElement);
+        a.forEach(function(item,i,a){
+            if(i!=index) {
+              item.classList.remove('active');
+            }
+         });
+
+
+         item.classList.toggle('active');
+
+         itemContent.forEach(function(item,i,a){
+            if(i!=index) {
+              item.classList.remove('active');
+            }
+         });
+         
+
+        
+         item.nextSibling.nextSibling.classList.toggle('active');
+         // item.parentElement.classList.toggle('active');
+
+    })
+
+  });
+  
+}
